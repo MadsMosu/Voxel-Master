116874d34513c5e7cb85366c90e03c7d1667c7ea
Mads <mads1108@hotmail.dk>
2020-02-26 16:28:03 +0100

    refactor DC and use Traverse method

--- Assets/VoxelMaster/Core/MeshGeneration/DualContouring/DualContouringGenerator.cs
+++ Assets/VoxelMaster/Core/MeshGeneration/DualContouring/DualContouringGenerator.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class DualContouring : VoxelMeshGenerator
-{
+public class DualContouring : VoxelMeshGenerator {
 
     private readonly static Vector2Int[] edges = new Vector2Int[12] {
         new Vector2Int (0, 1), new Vector2Int (0, 2), new Vector2Int (0, 4), new Vector2Int (1, 3), //x-axis
@@ -13,7 +12,6 @@ public class DualContouring : VoxelMeshGenerator
     };
 
     private readonly static Vector3Int[] vertOffsets = new Vector3Int[8] {
-
         new Vector3Int (0, 0, 0),
         new Vector3Int (0, 0, 1),
         new Vector3Int (0, 1, 0),
@@ -24,204 +22,144 @@ public class DualContouring : VoxelMeshGenerator
         new Vector3Int (1, 1, 1),
     };
 
-    private Vector3 Lerp(Vector3 v0, Vector3 v1, float t)
-    {
+    private readonly static Vector3Int[, ] directions = new Vector3Int[3, 3] { { new Vector3Int (1, 0, 0), new Vector3Int (1, 0, 1), new Vector3Int (0, 0, 1) }, { new Vector3Int (0, 1, 0), new Vector3Int (1, 1, 0), new Vector3Int (1, 0, 0) }, { new Vector3Int (0, 0, 1), new Vector3Int (0, 1, 1), new Vector3Int (0, 1, 0) }
+    };
+
+    private Vector3 Lerp (Vector3 v0, Vector3 v1, float t) {
         return (1 - t) * v0 + t * v1;
     }
 
-    private float InverseLerp(float a, float b, float t)
-    {
-        if (a != b)
-        {
+    private float InverseLerp (float a, float b, float t) {
+        if (a != b) {
             return (t - a / (b - a));
         }
         return 0;
     }
 
-    public override MeshData GenerateMesh(VoxelChunk chunk, Func<Vector3, float> densityFunction)
-    {
-        var vertices = new List<Vector3>();
-        var triangleIndicies = new List<int>();
-        var normals = new List<Vector3>();
-        var cellPoints = new Vector3[chunk.size.x * chunk.size.y * chunk.size.z];
-        var cellPointIndicies = new int[chunk.size.x * chunk.size.y * chunk.size.z];
+    private List<Vector3> normals;
+    private List<Vector3> vertices;
+    private List<int> triangleIndicies;
+    private Vector3[] cellPoints;
 
-        // for each cell that exhibits a sign change, a vertex is generated and positioned at the edge with the lowest error (using QEF)
-        for (int x = 0; x < chunk.size.x - 1; x++)
-            for (int y = 0; y < chunk.size.y - 1; y++)
-                for (int z = 0; z < chunk.size.z - 1; z++)
-                {
-                    Vector3Int cellPos = new Vector3Int(x, y, z);
-                    int corners = 0;
-                    for (int i = 0; i < 8; i++)
-                    {
-                        float density = chunk.voxels.GetVoxel(cellPos + vertOffsets[i]).density;
-                        if (density < chunk.isoLevel)
-                        {
-                            corners |= 1 << i;
-                        }
-                    }
-                    // if all of the corners are either outside or inside of surface boundary
-                    if (corners == 0 || corners == 255) continue;
+    public override MeshData GenerateMesh (VoxelChunk chunk, Func<Vector3, float> densityFunction) {
+        vertices = new List<Vector3> ();
+        triangleIndicies = new List<int> ();
+        normals = new List<Vector3> ();
+        cellPoints = new Vector3[chunk.size.x * chunk.size.y * chunk.size.z];
 
-                    QEF3D qef = new QEF3D();
-                    Vector3 averageNormal = new Vector3();
-                    for (int i = 0; i < edges.Length; i++)
-                    {
-                        Vector2Int edge = edges[i];
-                        int m1 = (corners >> edge.x) & 0b1;
-                        int m2 = (corners >> edge.y) & 0b1;
+        //for each cube that exhibits a sign change, a vertex is generated positioned at the minimizer of the QEF
+        chunk.voxels.Traverse (delegate (int x, int y, int z, Voxel v) {
+            GenerateCellVertex (new Vector3Int (x, y, z), chunk, densityFunction);
+        });
 
-                        // check for zero crossing point. the point of where the sign function changes (eg. positive to negative or negative to positive)
-                        // only continue if there is no zero crossing (position of the surface). no need to make vertex inside the cube when there is no surface border
-                        if (m1 == m2) continue;
+        // for each of the edges that went through a sign change, triangles are generated connecting the vertices of the four adjacent cells
+        chunk.voxels.Traverse (delegate (int x, int y, int z, Voxel v) {
+            ConnectCellVertices (new Vector3Int (x, y, z), chunk);
+        });
 
-                        Vector3Int aPos = cellPos + vertOffsets[edge.x];
-                        Vector3Int bPos = cellPos + vertOffsets[edge.y];
+        for (int i = 0; i < vertices.Count; i += 4) {
+            triangleIndicies.Add (i);
+            triangleIndicies.Add (i + 1);
+            triangleIndicies.Add (i + 2);
 
-                        Vector3 aOffset = vertOffsets[edge.x];
-                        Vector3 bOffset = vertOffsets[edge.y];
-
-                        float aDensity = chunk.voxels.GetVoxel(aPos).density;
-                        float bDensity = chunk.voxels.GetVoxel(bPos).density;
-
-                        var intersectionPoint = Lerp(aPos, bPos, InverseLerp(aDensity, bDensity, chunk.isoLevel));
-                        // var intersectionPoint = (aOffset + (-aDensity) * (bOffset - aOffset) / (bDensity - aDensity));
-
-                        var normal = GetNormal(intersectionPoint, densityFunction);
-                        averageNormal += normal;
-
-                        qef.Add(intersectionPoint, normal);
-                    }
-                    //the vertex that is clostest to the surface
-                    var vertex = qef.Solve();
-                    var averagedNormal = Vector3.Normalize(averageNormal / (float)qef.Intersections.Count);
-                    Vector3 c_v = averagedNormal * 0.5f + Vector3.one * 0.5f;
-                    c_v.Normalize();
-                    normals.Add(averagedNormal);
-
-                    // cellPointIndicies[Util.Map3DTo1D(cellPos, chunk.size)] = vertices.Count;
-                    cellPoints[Util.Map3DTo1D(cellPos, chunk.size)] = vertex;
-                    // vertices.Add(vertex);
-                }
-
-        // for each of the edges that went through a sign change, triangles are generated connecting the vertices of the four cubes adjacent to the edge
-        for (int x = 0; x < chunk.size.x - 1; x++)
-            for (int y = 0; y < chunk.size.y - 1; y++)
-                for (int z = 0; z < chunk.size.z - 1; z++)
-                {
-                    var cellPos = new Vector3Int(x, y, z);
-                    var v0 = cellPos;
-                    var v1 = v0 + new Vector3Int(1, 0, 0);
-                    var v2 = v0 + new Vector3Int(1, 0, 1);
-                    var v3 = v0 + new Vector3Int(0, 0, 1);
-
-                    Vector3 value0 = cellPoints[Util.Map3DTo1D(v0, chunk.size)];
-                    Vector3 value1 = cellPoints[Util.Map3DTo1D(v1, chunk.size)];
-                    Vector3 value2 = cellPoints[Util.Map3DTo1D(v2, chunk.size)];
-                    Vector3 value3 = cellPoints[Util.Map3DTo1D(v3, chunk.size)];
-
-                    if (value0 != Vector3.zero &&
-                        value1 != Vector3.zero &&
-                        value2 != Vector3.zero &&
-                        value3 != Vector3.zero)
-                    {
-                        if (chunk.voxels.GetVoxel(v2).density > 0)
-                        {
-                            vertices.Add(value0);
-                            vertices.Add(value1);
-                            vertices.Add(value2);
-                            vertices.Add(value3);
-                        }
-                        else
-                        {
-                            vertices.Add(value0);
-                            vertices.Add(value3);
-                            vertices.Add(value2);
-                            vertices.Add(value1);
-                        }
-                    }
-
-                    v1 = v0 + new Vector3Int(0, 1, 0);
-                    v2 = v0 + new Vector3Int(1, 1, 0);
-                    v3 = v0 + new Vector3Int(1, 0, 0);
-
-                    value1 = cellPoints[Util.Map3DTo1D(v1, chunk.size)];
-                    value2 = cellPoints[Util.Map3DTo1D(v2, chunk.size)];
-                    value3 = cellPoints[Util.Map3DTo1D(v3, chunk.size)];
-
-                    if (value0 != Vector3.zero &&
-                        value1 != Vector3.zero &&
-                        value2 != Vector3.zero &&
-                        value3 != Vector3.zero)
-                    {
-                        if (chunk.voxels.GetVoxel(v2).density > 0)
-                        {
-                            vertices.Add(value0);
-                            vertices.Add(value1);
-                            vertices.Add(value2);
-                            vertices.Add(value3);
-                        }
-                        else
-                        {
-                            vertices.Add(value0);
-                            vertices.Add(value3);
-                            vertices.Add(value2);
-                            vertices.Add(value1);
-                        }
-                    }
-
-                    v1 = v0 + new Vector3Int(0, 0, 1);
-                    v2 = v0 + new Vector3Int(0, 1, 1);
-                    v3 = v0 + new Vector3Int(0, 1, 0);
-
-                    value1 = cellPoints[Util.Map3DTo1D(v1, chunk.size)];
-                    value2 = cellPoints[Util.Map3DTo1D(v2, chunk.size)];
-                    value3 = cellPoints[Util.Map3DTo1D(v3, chunk.size)];
-
-                    if (value0 != Vector3.zero &&
-                        value1 != Vector3.zero &&
-                        value2 != Vector3.zero &&
-                        value3 != Vector3.zero)
-                    {
-                        if (chunk.voxels.GetVoxel(v2).density > 0)
-                        {
-                            vertices.Add(value0);
-                            vertices.Add(value1);
-                            vertices.Add(value2);
-                            vertices.Add(value3);
-                        }
-                        else
-                        {
-                            vertices.Add(value0);
-                            vertices.Add(value3);
-                            vertices.Add(value2);
-                            vertices.Add(value1);
-                        }
-                    }
-                }
-
-        for (int i = 0; i < vertices.Count; i += 4)
-        {
-            triangleIndicies.Add(i);
-            triangleIndicies.Add(i + 1);
-            triangleIndicies.Add(i + 2);
-
-            triangleIndicies.Add(i + 2);
-            triangleIndicies.Add(i + 3);
-            triangleIndicies.Add(i);
+            triangleIndicies.Add (i + 2);
+            triangleIndicies.Add (i + 3);
+            triangleIndicies.Add (i);
         }
-        return new MeshData(vertices.ToArray(), triangleIndicies.ToArray());
+        return new MeshData (vertices.ToArray (), triangleIndicies.ToArray ());
     }
 
-    private Vector3 GetNormal(Vector3 v, Func<Vector3, float> densityFunction)
-    {
-        float offset = 0.001f;
-        float dx = densityFunction(new Vector3(v.x + offset, v.y, v.z)) - densityFunction(new Vector3(v.x - offset, v.y, v.z));
-        float dy = densityFunction(new Vector3(v.x, v.y + offset, v.z)) - densityFunction(new Vector3(v.x, v.y - offset, v.z));
-        float dz = densityFunction(new Vector3(v.x, v.y, v.z + offset)) - densityFunction(new Vector3(v.x, v.y, v.z - offset));
+    private void GenerateCellVertex (Vector3Int cellPos, VoxelChunk chunk, Func<Vector3, float> densityFunction) {
+        if (cellPos.x + 1 >= chunk.size.x || cellPos.y + 1 >= chunk.size.y || cellPos.z + 1 >= chunk.size.z) return;
 
-        var gradient = new Vector3(-dx, -dy, -dz).normalized;
+        int corners = 0;
+        for (int i = 0; i < 8; i++) {
+            float density = chunk.voxels.GetVoxel (cellPos + vertOffsets[i]).density;
+            if (density < chunk.isoLevel) {
+                corners |= 1 << i;
+            }
+        }
+        // if all of the corners are either outside or inside of surface boundary
+        if (corners == 0 || corners == 255) return;
+
+        QEF3D qef = new QEF3D ();
+        Vector3 averageNormal = new Vector3 ();
+        for (int i = 0; i < edges.Length; i++) {
+            Vector2Int edge = edges[i];
+            int m1 = (corners >> edge.x) & 0b1;
+            int m2 = (corners >> edge.y) & 0b1;
+
+            // check for zero crossing point. the point of where the sign function changes (eg. positive to negative or negative to positive)
+            // only continue if there is no zero crossing (position of the surface). no need to make vertex inside the cube when there is no surface border
+            if (m1 == m2) continue;
+
+            Vector3Int aPos = cellPos + vertOffsets[edge.x];
+            Vector3Int bPos = cellPos + vertOffsets[edge.y];
+
+            Vector3 aOffset = vertOffsets[edge.x];
+            Vector3 bOffset = vertOffsets[edge.y];
+
+            float aDensity = chunk.voxels.GetVoxel (aPos).density;
+            float bDensity = chunk.voxels.GetVoxel (bPos).density;
+
+            var intersectionPoint = Lerp (aPos, bPos, InverseLerp (aDensity, bDensity, chunk.isoLevel));
+            // var intersectionPoint = (aOffset + (-aDensity) * (bOffset - aOffset) / (bDensity - aDensity));
+
+            var normal = GetNormal (intersectionPoint, densityFunction);
+            averageNormal += normal;
+
+            qef.Add (intersectionPoint, normal);
+        }
+        //the vertex that is clostest to the surface
+        var vertex = qef.Solve ();
+        var averagedNormal = Vector3.Normalize (averageNormal / (float) qef.Intersections.Count);
+        Vector3 c_v = averagedNormal * 0.5f + Vector3.one * 0.5f;
+        c_v.Normalize ();
+        normals.Add (averagedNormal);
+
+        cellPoints[Util.Map3DTo1D (cellPos, chunk.size)] = vertex;
+    }
+
+    private void ConnectCellVertices (Vector3Int cellPos, VoxelChunk chunk) {
+        if (cellPos.x + 1 >= chunk.size.x || cellPos.y + 1 >= chunk.size.y || cellPos.z + 1 >= chunk.size.z) return;
+        var v0 = cellPos;
+
+        for (int i = 0; i < 3; i++) {
+            var v1 = v0 + directions[i, 0];
+            var v2 = v0 + directions[i, 1];
+            var v3 = v0 + directions[i, 2];
+
+            Vector3 value0 = cellPoints[Util.Map3DTo1D (v0, chunk.size)];
+            Vector3 value1 = cellPoints[Util.Map3DTo1D (v1, chunk.size)];
+            Vector3 value2 = cellPoints[Util.Map3DTo1D (v2, chunk.size)];
+            Vector3 value3 = cellPoints[Util.Map3DTo1D (v3, chunk.size)];
+
+            if (value0 != Vector3.zero &&
+                value1 != Vector3.zero &&
+                value2 != Vector3.zero &&
+                value3 != Vector3.zero) {
+                if (chunk.voxels.GetVoxel (v2).density > 0) {
+                    vertices.Add (value0);
+                    vertices.Add (value1);
+                    vertices.Add (value2);
+                    vertices.Add (value3);
+                } else {
+                    vertices.Add (value0);
+                    vertices.Add (value3);
+                    vertices.Add (value2);
+                    vertices.Add (value1);
+                }
+            }
+        }
+    }
+
+    private Vector3 GetNormal (Vector3 v, Func<Vector3, float> densityFunction) {
+        float offset = 0.001f;
+        float dx = densityFunction (new Vector3 (v.x + offset, v.y, v.z)) - densityFunction (new Vector3 (v.x - offset, v.y, v.z));
+        float dy = densityFunction (new Vector3 (v.x, v.y + offset, v.z)) - densityFunction (new Vector3 (v.x, v.y - offset, v.z));
+        float dz = densityFunction (new Vector3 (v.x, v.y, v.z + offset)) - densityFunction (new Vector3 (v.x, v.y, v.z - offset));
+
+        var gradient = new Vector3 (-dx, -dy, -dz).normalized;
         return gradient;
     }
 }
--- Assets/VoxelMaster/Core/MeshGeneration/MarchingCubes/MarchingCubesGenerator.cs
+++ Assets/VoxelMaster/Core/MeshGeneration/MarchingCubes/MarchingCubesGenerator.cs
@@ -2,59 +2,54 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MarchingCubes : VoxelMeshGenerator
-{
-    public override MeshData GenerateMesh(VoxelChunk chunk, Func<Vector3, float> densityFunction)
-    {
+public class MarchingCubes : VoxelMeshGenerator {
+    public override MeshData GenerateMesh (VoxelChunk chunk, Func<Vector3, float> densityFunction) {
         int numCells = chunk.size.x * chunk.size.y * chunk.size.z;
-        var vertices = new List<Vector3>(5 * numCells * 3);
-        var triangleIndicies = new List<int>(5 * numCells * 3);
+        var vertices = new List<Vector3> (5 * numCells * 3);
+        var triangleIndicies = new List<int> (5 * numCells * 3);
         int triangleIndex = 0;
 
-        for (int x = 0; x < chunk.size.x - 1; x++)
-            for (int y = 0; y < chunk.size.y - 1; y++)
-                for (int z = 0; z < chunk.size.z - 1; z++)
-                {
-                    Vector3Int cellPos = new Vector3Int(x, y, z);
-                    float[] cubeDensity = new float[8] {
-                        chunk.voxels.GetVoxel (cellPos + Lookup.cubeVertOffsets[0]).density,
-                        chunk.voxels.GetVoxel (cellPos + Lookup.cubeVertOffsets[1]).density,
-                        chunk.voxels.GetVoxel (cellPos + Lookup.cubeVertOffsets[2]).density,
-                        chunk.voxels.GetVoxel (cellPos + Lookup.cubeVertOffsets[3]).density,
-                        chunk.voxels.GetVoxel (cellPos + Lookup.cubeVertOffsets[4]).density,
-                        chunk.voxels.GetVoxel (cellPos + Lookup.cubeVertOffsets[5]).density,
-                        chunk.voxels.GetVoxel (cellPos + Lookup.cubeVertOffsets[6]).density,
-                        chunk.voxels.GetVoxel (cellPos + Lookup.cubeVertOffsets[7]).density,
-                    };
+        chunk.voxels.Traverse (delegate (int x, int y, int z, Voxel v) {
+            var cellPos = new Vector3Int (x, y, z);
+            if (cellPos.x + 1 >= chunk.size.x || cellPos.y + 1 >= chunk.size.y || cellPos.z + 1 >= chunk.size.z) return;
 
-                    int cubeindex = 0;
-                    if (cubeDensity[0] < chunk.isoLevel) cubeindex |= 1;
-                    if (cubeDensity[1] < chunk.isoLevel) cubeindex |= 2;
-                    if (cubeDensity[2] < chunk.isoLevel) cubeindex |= 4;
-                    if (cubeDensity[3] < chunk.isoLevel) cubeindex |= 8;
-                    if (cubeDensity[4] < chunk.isoLevel) cubeindex |= 16;
-                    if (cubeDensity[5] < chunk.isoLevel) cubeindex |= 32;
-                    if (cubeDensity[6] < chunk.isoLevel) cubeindex |= 64;
-                    if (cubeDensity[7] < chunk.isoLevel) cubeindex |= 128;
+            float[] cubeDensity = new float[8] {
+                chunk.voxels.GetVoxel (cellPos + Lookup.cubeVertOffsets[0]).density,
+                chunk.voxels.GetVoxel (cellPos + Lookup.cubeVertOffsets[1]).density,
+                chunk.voxels.GetVoxel (cellPos + Lookup.cubeVertOffsets[2]).density,
+                chunk.voxels.GetVoxel (cellPos + Lookup.cubeVertOffsets[3]).density,
+                chunk.voxels.GetVoxel (cellPos + Lookup.cubeVertOffsets[4]).density,
+                chunk.voxels.GetVoxel (cellPos + Lookup.cubeVertOffsets[5]).density,
+                chunk.voxels.GetVoxel (cellPos + Lookup.cubeVertOffsets[6]).density,
+                chunk.voxels.GetVoxel (cellPos + Lookup.cubeVertOffsets[7]).density,
+            };
 
-                    int[] triangulation = Lookup.triTable[cubeindex];
-                    for (int i = 0; triangulation[i] != -1; i += 3)
-                    {
-                        for (int j = 0; j < 3; j++)
-                        {
-                            var a = Lookup.cornerIndexAFromEdge[triangulation[i + j]];
-                            var b = Lookup.cornerIndexBFromEdge[triangulation[i + j]];
+            int cubeindex = 0;
+            if (cubeDensity[0] < chunk.isoLevel) cubeindex |= 1;
+            if (cubeDensity[1] < chunk.isoLevel) cubeindex |= 2;
+            if (cubeDensity[2] < chunk.isoLevel) cubeindex |= 4;
+            if (cubeDensity[3] < chunk.isoLevel) cubeindex |= 8;
+            if (cubeDensity[4] < chunk.isoLevel) cubeindex |= 16;
+            if (cubeDensity[5] < chunk.isoLevel) cubeindex |= 32;
+            if (cubeDensity[6] < chunk.isoLevel) cubeindex |= 64;
+            if (cubeDensity[7] < chunk.isoLevel) cubeindex |= 128;
 
-                            Vector3Int aPos = cellPos + Lookup.cubeVertOffsets[a];
-                            Vector3Int bPos = cellPos + Lookup.cubeVertOffsets[b];
-                            float lerp = (chunk.isoLevel - cubeDensity[a]) / (cubeDensity[b] - cubeDensity[a]);
-                            var vertex = Vector3.Lerp(aPos, bPos, lerp);
+            int[] triangulation = Lookup.triTable[cubeindex];
+            for (int i = 0; triangulation[i] != -1; i += 3) {
+                for (int j = 0; j < 3; j++) {
+                    var a = Lookup.cornerIndexAFromEdge[triangulation[i + j]];
+                    var b = Lookup.cornerIndexBFromEdge[triangulation[i + j]];
 
-                            vertices.Add(vertex);
-                            triangleIndicies.Add(triangleIndex++);
-                        }
-                    }
+                    Vector3Int aPos = cellPos + Lookup.cubeVertOffsets[a];
+                    Vector3Int bPos = cellPos + Lookup.cubeVertOffsets[b];
+                    float lerp = (chunk.isoLevel - cubeDensity[a]) / (cubeDensity[b] - cubeDensity[a]);
+                    var vertex = Vector3.Lerp (aPos, bPos, lerp);
+
+                    vertices.Add (vertex);
+                    triangleIndicies.Add (triangleIndex++);
                 }
-        return new MeshData(vertices.ToArray(), triangleIndicies.ToArray());
+            }
+        });
+        return new MeshData (vertices.ToArray (), triangleIndicies.ToArray ());
     }
 }
