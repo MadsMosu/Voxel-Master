0d0c30862dda8c163cba8a39954acd74a5c9a71a
Mads <mads1108@hotmail.dk>
2020-04-22 09:57:29 +0200

    transvoxel GPU init

---
+++ Assets/VoxelMaster/Core/MeshGeneration/MarchingCubes/enhanced/GPU/MarchingCubesEnhancedGPU.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class MarchingCubesEnhancedGPU : VoxelMeshGenerator {
+    public override MeshData GenerateMesh (IVoxelData volume, Vector3Int origin, int step, float scale) {
+        throw new System.NotImplementedException ();
+    }
+
+    public override void Init (MeshGeneratorSettings settings) {
+        throw new System.NotImplementedException ();
+    }
+}
